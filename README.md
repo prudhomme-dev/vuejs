119435704514288a95b46cadc6e4e7964e68e3ed
Cedric P <cedric.prudhomme@protonmail.com>
2022-02-23 14:28:22 +0100

    Exercice CV - CSS - Enregistrement Local

---
+++ README.md
@@ -0,0 +1,13 @@
+# VueJS AP Formation Toulouse
+
+Exercices lié à la formation de VueJS au sein du centre de formation : AP Formation à Toulouse.
+
+index.html : correspondant 
+initiation-exo.html et appcv.js : Correspondant aux exercices d'initiations du cours VueJS
+cv.html / appcv.js / cv.css : Correspondant à l'exercice CV du cours d'initiation à VueJS
+
+
+## Auteur
+
+- [@prudhomme-dev](https://github.com/prudhomme-dev)
+
--- appcv.js
+++ appcv.js
@@ -1,10 +1,15 @@
-let Information = {
-    template: `<section>
+let information = {
+    template: `<section class="information">
+    <h2 class="poste">{{intitule|capitale}}</h2>
     <h2>{{nom|capitale}} {{prenom | capitaleprem}}</h2>
-    <p v-if="age">Age : {{age}}</p>
+    <div class="flex">
+    <img v-if="urlphoto" :src="urlphoto" :alt="nom">
+    <div class="info">
+    <p v-if="age">Age : {{age}} ({{datenaissance|dateFr}})</p>
     <p v-if="adresse">Adresse : {{adresse}}</p>
     <p v-if="!voiture" style="color:red;">Voiture : Non</p>
-    <p v-else style="color:green;">Voiture : Oui</p>
+    <p v-else style="color:green;">Voiture : Oui</p></div>
+    </div>
     </section>`,
     filters: {
         capitale: function (value) {
@@ -12,6 +17,10 @@ let Information = {
         },
         capitaleprem: function (value) {
             return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        dateFr: function (value) {
+            dateAChanger = new Date(value);
+            return dateAChanger.toLocaleDateString("fr-FR");
         }
     },
     computed: {
@@ -22,41 +31,121 @@ let Information = {
 
         }
     },
-    props: ['nom', 'prenom', 'datenaissance', 'adresse', 'voiture']
+    props: ['nom', 'prenom', 'datenaissance', 'adresse', 'voiture', 'intitule', 'urlphoto']
 }
 
 let ExperiencePro = {
-    template: `<section v-if="experiencepro.length > 0">
+    template: `<section v-if="experiencepro.length > 0" class="experiencePro">
     <h2>Expériences Professionnelles</h2>
     <ul>
-        <li v-for="ex of experiencepro">{{ex.experience}} - du {{ex.dateDebut}} au {{ex.dateFin}}</li>
+        <li v-for="ex of experiencepro">{{ex.experience}} - du {{ex.dateDebut|dateFr}} au {{ex.dateFin|dateFr}} - {{ex.intPoste}}</li>
 
     </ul>
     </section>`,
-    props: ['experiencepro']
+    props: ['experiencepro'],
+    filters: {
+        dateFr: function (value) {
+            dateAChanger = new Date(value);
+            return dateAChanger.toLocaleDateString("fr-FR");
+        }
+    }
 }
 
 
 let Formation = {
-    template: `<section v-if="formations.length > 0">
+    template: `<section v-if="formations.length > 0" class="formation">
     <h2>Formation</h2>
     <ul>
-        <li v-for="fm of formations">{{fm.nomFormation}} - du {{fm.dateDebut}} au {{fm.dateFin}}</li>
+        <li v-for="fm of formations">{{fm.nomFormation}} - du {{fm.dateDebut|dateFr}} au {{fm.dateFin|dateFr}}</li>
 
     </ul>
     </section>`,
-    props: ['formations']
+    props: ['formations'],
+    filters: {
+        dateFr: function (value) {
+            dateAChanger = new Date(value);
+            return dateAChanger.toLocaleDateString("fr-FR");
+        }
+    }
 }
 
 let Competences = {
-    template: `<section v-if="competences.length > 0">
+    template: `<section v-if="competences.length > 0" class="competence">
     <h2>Compétences</h2>
     <ul>
-        <li v-for="cp of competences">{{cp}}</li>
+        <li v-for="cp of competences">{{cp | capitaleprem}}</li>
 
     </ul>
     </section>`,
+    filters: {
+        capitaleprem: function (value) {
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        }
+    },
     props: ['competences']
+}
+
+let listecompetences = {
+    template: `
+    <section v-if="competences.length > 0" class="competence">
+    <p> Supprimer une compétence : 
+    <select id="listeCompetences">
+    <option v-for="competence of competences" :value="competence">{{competence}}</option>
+    </select>
+    <button v-on:click="competenceSupprimer()">Supprimer</button>
+    </p>
+    </section>
+    `,
+    props: ['competences'],
+    methods: {
+        competenceSupprimer: function () {
+            if (confirm("Voulez-vous supprimer la compétence " + document.querySelector("#listeCompetences").value + " ?")) {
+                this.competences.splice(document.querySelector("#listeCompetences").selectedIndex, 1)
+            }
+        }
+    }
+}
+
+let listeformations = {
+    template: `
+    <section v-if="formations.length > 0" class="formation">
+    <p> Supprimer une formation : 
+    <select id="listeFormations">
+    <option v-for="formation of formations" :value="formation.nomFormation">{{formation.nomFormation}}</option>
+    </select>
+    <button v-on:click="formationSupprimer()">Supprimer</button>
+    </p>
+    </section>
+    `,
+    props: ['formations'],
+    methods: {
+        formationSupprimer: function () {
+            if (confirm("Voulez-vous supprimer la formation " + document.querySelector("#listeFormations").value + " ?")) {
+                this.formations.splice(document.querySelector("#listeFormations").selectedIndex, 1)
+            }
+        }
+    }
+}
+
+let listeexppro = {
+    template: `
+    <section v-if="experiences.length > 0" class="formation">
+    <p> Supprimer une expérience pro : 
+    <select id="listeExperiences">
+    <option v-for="experience of experiences" :value="experience.experience">{{experience.experience}}</option>
+    </select>
+    <button v-on:click="experienceSupprimer()">Supprimer</button>
+    </p>
+    </section>
+    `,
+    props: ['experiences'],
+    methods: {
+        experienceSupprimer: function () {
+            if (confirm("Voulez-vous supprimer l'expérience professionnelle : " + document.querySelector("#listeExperiences").value + " ?")) {
+                this.experiences.splice(document.querySelector("#listeExperiences").selectedIndex, 1)
+            }
+        }
+    }
 }
 
 
@@ -70,47 +159,53 @@ let app = new Vue({
         voiture: false,
         experiencePro: [],
         formation: [],
-        competence: []
+        competence: [],
+        intitulePoste: '',
+        urlPhoto: 'https://dummyimage.com/120x120/000000/ffffff.png'
     },
     created: function () {
         let data = JSON.parse(localStorage.getItem('cv'))
-        this.nom = data.nom
-        this.prenom = data.prenom
-        this.dateDeNaissance = data.dateDeNaissance
-        this.adresse = data.adresse
-        this.voiture = data.voiture
-        this.experiencePro = data.experiencePro
-        this.formation = data.formation
-        this.competence = data.competence
+        for (datakey in data) {
+            this.$data[datakey] = data[datakey]
+        }
 
     },
     components: {
-        'informations': Information,
+        information,
         'experiencepro': ExperiencePro,
         'formation': Formation,
-        'competence': Competences
+        'competence': Competences,
+        listecompetences,
+        listeformations,
+        listeexppro
     },
     methods: {
         saveStorage: function (key, data) {
-            data = app._data
-            let jsonTask = JSON.stringify(data)
-            localStorage.setItem(key, jsonTask)
+            data = app.$data;
+            let jsonTask = JSON.stringify(data);
+            localStorage.setItem(key, jsonTask);
+            alert("CV Sauvegardé");
         },
         delStorage: function (key) {
-            localStorage.removeItem(key);
+            if (confirm("Voulez-vous supprimer toutes les données ?")) {
+                localStorage.removeItem(key);
+                location.reload()
+            }
 
         },
         ajouterExpPro: function () {
             let exp = document.querySelector('#experience').value;
             let dateDeb = document.querySelector('#expDateDebut').value
             let dateF = document.querySelector('#expDateFin').value
-            if (exp && dateDeb && dateF && dateF > dateDeb) {
+            let intPoste = document.querySelector('#expIntitulePoste').value
+            if (exp && dateDeb && dateF && dateF > dateDeb && intPoste) {
 
 
                 this.experiencePro.push({
                     experience: exp,
                     dateDebut: dateDeb,
-                    dateFin: dateF
+                    dateFin: dateF,
+                    intPoste: intPoste
                 })
                 document.querySelector('#experience').value = ''
                 document.querySelector('#expDateDebut').value = ''
@@ -143,6 +238,9 @@ let app = new Vue({
                 this.competence.push(comp)
                 document.querySelector('#competence').value = ''
             }
+        },
+        printCV: function () {
+            window.print()
         }
     }
 })
